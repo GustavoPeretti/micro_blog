3bccb441ea1f0d69a9c58f0adede883b92ec2bce
Gustavo Peretti <gustavoperetti11@gmail.com>
2024-07-08 11:18:37 -0300

    Commit da bagunça

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,32 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', dados={'nome': request.args.get('nome'), 'email': request.args.get('email')})
+
+@app.route('/usuario/<nome>')
+def usuario(nome):
+    return nome
+
+@app.route('/ano/<int:x>')
+def ano(x):
+    return 'Estamos no ' + str(x) + 'º ano.'
+
+@app.route('/nota/<float:x>')
+def nota(x):
+    return f'Nota: {x}.'
+
+@app.route('/media/<float:x>/<float:y>')
+def media(x, y):
+    return f'Média: {(x + y) / 2}.'
+
+@app.route('/soma/<x>/<y>')
+def soma(x, y):
+    try:
+        x = float(x)
+        y = float(y)
+    except:
+        return 'Erro: forneça valores válidos.'
+    return render_template('somar.html', dados={'x': x, 'y': y})
