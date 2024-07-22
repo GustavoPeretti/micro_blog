199b27001b79b1b82b2affc9f8c2ec1eb098c9fb
Gustavo Peretti <gustavoperetti11@gmail.com>
2024-07-22 11:12:43 -0300

    Página inicial

--- app/routes.py
+++ app/routes.py
@@ -1,32 +1,20 @@
 from app import app
 from flask import render_template, request
 
+class Usuario:
+    def __init__(self, nome, descricao, status):
+        self.nome = nome
+        self.descricao = descricao
+        self.status = status
+
+usuario = Usuario('Gustavo', 'Aluno do IFC', True)
+usuarios = [Usuario('Galileu Galilei', 'negocio é heliocentrismo', True), Usuario('Charles Darwin', 'os que mais se adapta sobrevive', False), Usuario('Tchaikovsky', 'quebra nozes', False)]
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', dados={'nome': request.args.get('nome'), 'email': request.args.get('email')})
+    return render_template('index.html', dados={'usuario': usuario, 'usuarios': usuarios})
 
-@app.route('/usuario/<nome>')
-def usuario(nome):
-    return nome
-
-@app.route('/ano/<int:x>')
-def ano(x):
-    return 'Estamos no ' + str(x) + 'º ano.'
-
-@app.route('/nota/<float:x>')
-def nota(x):
-    return f'Nota: {x}.'
-
-@app.route('/media/<float:x>/<float:y>')
-def media(x, y):
-    return f'Média: {(x + y) / 2}.'
-
-@app.route('/soma/<x>/<y>')
-def soma(x, y):
-    try:
-        x = float(x)
-        y = float(y)
-    except:
-        return 'Erro: forneça valores válidos.'
-    return render_template('somar.html', dados={'x': x, 'y': y})
+@app.route('/contatos')
+def contato():
+    return render_template('contatos.html', contatos=[])
