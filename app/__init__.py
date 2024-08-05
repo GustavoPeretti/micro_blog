ccf1acb90a3d8fab497561fdb830cfdfb4dbb069
Gustavo Peretti <gustavoperetti11@gmail.com>
2024-08-05 10:22:01 -0300

    Login

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
+app.secret_key = os.environ.get('SECRET_KEY')
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for, session
 
 class Usuario:
     def __init__(self, nome, descricao, status):
@@ -7,14 +7,24 @@ class Usuario:
         self.descricao = descricao
         self.status = status
 
-usuario = Usuario('Gustavo', 'Aluno do IFC', True)
 usuarios = [Usuario('Galileu Galilei', 'negocio é heliocentrismo', True), Usuario('Charles Darwin', 'os que mais se adapta sobrevive', False), Usuario('Tchaikovsky', 'quebra nozes', False)]
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 @app.route('/index')
 def index():
-    return render_template('index.html', dados={'usuario': usuario, 'usuarios': usuarios})
+    usuario = Usuario(request.args.get('usuario'), request.args.get('descricao'), True)
+    return render_template('index.html', usuario=usuario, usuarios=usuarios)
 
-@app.route('/contatos')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    if request.method == 'POST':
+        session['usuario'] = request.form.get('usuario')
+        session['descricao'] = request.form.get('descricao')
+        return redirect(url_for('index'))
+
+@app.route('/contatos', methods=['GET'])
 def contato():
     return render_template('contatos.html', contatos=[])
+
